4e33e954180bc692f0f6b0ec63257090e5cc56f1
Alexandre Bourcier <alexandre.bourcier@epita.fr>
2020-10-05 13:38:08 +0200

    SDL

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 main : main.c
-	gcc `pkg-config --cflags gtk+-3.0` main.c -o test `pkg-config --libs gtk+-3.0`
+	gcc main.c -o test `sdl2-config --cflags --libs`
--- main.c
+++ main.c
@@ -1,28 +1,26 @@
-#include <gtk/gtk.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
+
+int main (int    argc, char **argv)
 {
-  GtkWidget *window;
+	if(SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		fprintf(stdout,"Echec de l'initialisation de SDL");
+		return -1;
+	}
+	SDL_Window* pWindow = NULL;
+	pWindow = SDL_CreateWindow("fenetre nulle", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN);
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
+	if(pWindow)
+	{
+          SDL_Delay(3000);
+          SDL_DestroyWindow(pWindow);
+  }
+  else 
+  {
+          fprintf(stderr,"Erreur lors de la creation de la fenetre");
+  }
+  SDL_Quit();
 }
 
-int
-main (int    argc,
-      char **argv)
-{
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
-}
