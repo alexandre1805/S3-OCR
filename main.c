35868dd96d672031a9e2c187b4b664f8db23b05b
Alexandre Bourcier <alexandre.bourcier@epita.fr>
2020-10-05 14:39:11 +0200

    renderer

--- main.c
+++ main.c
@@ -10,17 +10,25 @@ int main (int    argc, char **argv)
 		return -1;
 	}
 	SDL_Window* pWindow = NULL;
+	SDL_Renderer* ren = NULL;
 	pWindow = SDL_CreateWindow("fenetre nulle", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN);
 
-	if(pWindow)
+	if(pWindow == NULL)
+  	{
+		fprintf(stderr,"Erreur lors de la creation de la fenetre");
+		return -1;
+  	}
+
+	ren = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if(ren == NULL)
 	{
-          SDL_Delay(3000);
-          SDL_DestroyWindow(pWindow);
-  }
-  else 
-  {
-          fprintf(stderr,"Erreur lors de la creation de la fenetre");
-  }
-  SDL_Quit();
+		fprintf(stdout,"Erreur lors de la creation du renderer ");
+	}
+	SDL_Delay(3000);
+
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(pWindow);
+  	SDL_Quit();
 }
 
