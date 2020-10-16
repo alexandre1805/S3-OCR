6cfdfcae6d8ea4f86c5f7258cb5a9e2c50cda93a
clement.bruley <clement.bruley@epita.fr>
2020-10-16 19:07:12 +0200

    matrix

---
+++ Sdl_To_Mat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+long[][] Sdl_To_Matrix(SDL_SURFACE image)
+{
+	int w = image -> w;
+	int h = image -> h;
+	long mat[w][h];
+	for(int i=0;i<w;i++)
+	{
+		for(int j=0;j<h;j++)
+		{
+			Uint32 pix =Get_pixel(image,i,j);
+			Uint8 r=0;
+			Uint8 g=0;
+			Uint8 b=0;
+			SDL_GetRGB(pix,image -> format,&r,&g,&b);
+			if (r==0)
+				mat[i][j]=1;
+			else
+				mat[i][j]=0;
+		}
+	}
+	return mat;
+}
+
---
+++ Sdl_To_Mat.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+long[][] Sdl_To_Matrix(SDL_SURFACE image);
